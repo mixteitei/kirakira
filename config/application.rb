b2b2020b1ca3988a6f08bfefa817e87e83c9e973
mixteitei <jittoshiteruyo@gmail.com>
2021-12-27 16:18:04 +0000

    add time

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,10 @@ module Kirakira
     config.load_defaults 5.2
     config.paths.add 'lib', eager_load: true
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
